f526aa9b0b7d0c454ff81470e2948e31c76a2fff
FScaccheri <francocass2009@hotmail.com>
2019-06-22 18:08:26 -0300

    Vocabulary list can redirect to detail view

--- jcomp/apps/vocabulary/urls.py
+++ jcomp/apps/vocabulary/urls.py
@@ -4,5 +4,5 @@ from .views import VocabularyListView, WordDetailView
 
 urlpatterns = [
     re_path(r'^$', VocabularyListView.as_view(), name='vocabulary_list'),
-    re_path(r'^(?P<slug>\w+)/$', WordDetailView.as_view(), name='word_detail')
+    path('<slug:slug>', WordDetailView.as_view(), name='word_detail')
 ]
